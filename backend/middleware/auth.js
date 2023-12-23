f8fb42e43171fd94ecfffb32045928dcbad98e47
GOR-OM <202101484@daiict.ac.in>
2023-12-24 02:24:26 +0530

    created isAuth function

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+import catchAsyncError from "./catchAsyncError.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
+import {User} from "../models/userModel.js";
+const isAuth = catchAsyncError(async (req,res,next) =>{
+    const { token } = await req.cookies;
+    if (!token) {
+        return next(new ErrorHandler("Login first to access this resource.",401));
+    }
+
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+
+
+}) 
+
+export default isAuth;
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express, {Router} from 'express';
 import { createProduct, deleteProduct, getAllProducts, getProductDetails, updateProduct } from '../controllers/productController.js';
+import  isAuth  from '../middleware/auth.js';
 const router = express.Router();
 
 
-router.route('/').get(getAllProducts) ;
-router.route('/new').post(createProduct) ;
-router.route('/:id').put(updateProduct).delete(deleteProduct).get(getProductDetails) ;
+router.route('/').get(isAuth ,getAllProducts) ;
+router.route('/new').post(isAuth ,createProduct) ;
+router.route('/:id').put(isAuth,updateProduct).delete(isAuth,deleteProduct).get(isAuth,getProductDetails) ;
 
 
 
