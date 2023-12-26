d5f32ac516a89bce8ad3756a3fecbb38ee1a1d4f
GOR-OM <202101484@daiict.ac.in>
2023-12-26 23:18:39 +0530

    set route if uodate password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express  from 'express';
-import { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile } from '../controllers/userControler.js';
+import { registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile,updatePassword } from '../controllers/userControler.js';
 import  isAuth  from '../middleware/auth.js';
 
 const router = express.Router();
@@ -12,6 +12,7 @@ router.route('/forgotpassword').post(forgotPassword);
 router.route('/resetpassword/:token').put(resetPassword);
 
 router.route("/me").get(isAuth,getUserProfile);
+router.route("/update").put(isAuth,updatePassword);
 
 
 export default router;
