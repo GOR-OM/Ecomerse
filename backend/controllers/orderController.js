69523c480a054dd5b903efc2c1e9d053f6ad1fcb
GOR-OM <202101484@daiict.ac.in>
2023-12-27 23:30:42 +0530

    created new order api

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,41 @@
-import { Order}  from "../models/orederModels";
-
+import { Order}  from "../models/orederModels.js";
+import {Product } from "../models/productModels.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import catchAsyncError from '../middleware/catchAsyncError.js';
 import ApiFeatures from '../utils/apiFeatures.js';
+
+
+// Create a new order   =>  /api/v1/order/new
+
+export const newOrder = catchAsyncError(async (req, res, next) => {
+
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
+
+    } = req.body;
+
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user._id
+
+    })
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,11 @@
 import express  from 'express';
 import  isAuth , {authorizeRole}  from '../middleware/auth.js';
+import { newOrder } from '../controllers/orderController.js';
 
 
 const router = express.Router();
+
+router.route('/new').get(isAuth,newOrder);
 
 
 
